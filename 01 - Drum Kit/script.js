350dd6470ca2a04c19de3f05fe722eaa1c655399
Marri Stephen <stephenmarri7@gmail.com>
2020-05-27 20:23:02 +0530

    JS1: Drum Kit Complete

---
+++ 01 - Drum Kit/script.js
@@ -0,0 +1,54 @@
+window.addEventListener('load',main);
+
+function main(){
+    
+    function playTransition(e){
+        const ele = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+        if(!ele) return;
+        audio.currentTime=0;
+        audio.play();
+        ele.classList.add('playing');  
+        
+    }
+
+    function removeTransition(e){
+        if(e.propertyName !== 'transform') return;
+        this.classList.remove('playing')
+    }
+    
+    const keys = document.querySelectorAll('.key')
+    keys.forEach(element => {
+        element.addEventListener('transitionend', removeTransition);
+    });
+
+    window.addEventListener('keydown',playTransition);
+
+
+
+    const mobile = document.querySelector('#mobile')
+
+    function myFunction(x) {
+        if (x.matches) { // If media query matches          
+          mobile.style.display="block";
+          mobile.addEventListener('change',emptyValue);          
+        } 
+      }
+      
+      function emptyValue(){
+          mobile.value="";
+      }
+
+      var x = window.matchMedia("(max-width: 500px)")
+      myFunction(x) // Call listener function at run time
+      
+
+
+
+
+
+
+
+
+
+}
